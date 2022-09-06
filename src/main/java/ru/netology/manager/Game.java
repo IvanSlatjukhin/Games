e21e2ba1d2d3793ebc33f0c7460edc6ca01931b7
IvanSlatjukhin <slatjukhin@mail.ru>
2022-09-06 20:55:20 +0300

    New com

--- src/main/java/ru/netology/manager/Game.java
+++ src/main/java/ru/netology/manager/Game.java
@@ -5,43 +5,43 @@ import ru.netology.exceptions.*;
 
 import java.util.ArrayList;
 
-public  class Game {
-private ArrayList<Player> players = new ArrayList<>();
+public class Game {
+    private ArrayList<Player> players = new ArrayList<>();
 
-public ArrayList<Player> getPlayers() {
+    public ArrayList<Player> getPlayers() {
         return players;
-        }
+    }
 
-public void register(Player player) {
+    public void register(Player player) {
         players.add(player);
-        }
+    }
 
-public int round(String playerName1, String playerName2) {
+    public int round(String playerName1, String playerName2) {
         int result = 0;
         if (findByName(playerName1) == null) {
-        throw new NotRegisteredException("The player " + playerName1 + " is not registered");
+            throw new NotRegisteredException("The player " + playerName1 + " is not registered");
         }
         if (findByName(playerName2) == null) {
-        throw new NotRegisteredException("The player " + playerName2 + " is not registered");
+            throw new NotRegisteredException("The player " + playerName2 + " is not registered");
         }
         if (findByName(playerName1).getStrength() < findByName(playerName2).getStrength()) {
-        result = 2;
+            result = 2;
         }
         if (findByName(playerName1).getStrength() > findByName(playerName2).getStrength()) {
-        result = 1;
+            result = 1;
         }
         if (findByName(playerName1).getStrength() == findByName(playerName2).getStrength()) {
-        result = 0;
+            result = 0;
         }
         return result;
-        }
+    }
 
-public Player findByName(String name) {
+    public Player findByName(String name) {
         for (Player player : players) {
-        if (player.getName() == name) {
-        return player;
-        }
+            if (player.getName() == name) {
+                return player;
+            }
         }
         return null;
-        }
+    }
 }
